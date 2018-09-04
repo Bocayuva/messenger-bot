eade68d88d6778e1a9ae45dc5684917bd7aa436c
Bocayuva <carlos.bocayuva@accenture.com>
2018-09-04 17:08:28 -0300

    change port

--- src/app.js
+++ src/app.js
@@ -19,4 +19,4 @@ app.get('/webhook', webhook.facebookVerification);
 app.post('/webhook', webhook.facebook);
 
 const server = http.createServer(app);
-server.listen(443, () => console.log('ADCC has started | port:', 443));
+server.listen(3000, () => console.log('ADCC has started | port:', 3000));
